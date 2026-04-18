e1af05c2209c6aa4278936c796088c542bd3a3cf
감성개발자 <nug22kr@gmail.com>
2026-04-18 19:49:28 +0900

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function RequireAuth({ children }) {
 
 export default function App() {
   return (
-    <HashRouter>
+    <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<RequireAuth><Layout /></RequireAuth>}>
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/global.css';
 import './styles/warehouse.css';
-import './build-time.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
